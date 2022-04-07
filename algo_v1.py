421624d42c7467f5c7a8d5277ac49c9baa8ed61f
lvlconcepts <leo.niss@student.fh-kiel.de>
2022-04-07 22:36:06 +0200

    sort merch algov1

--- algo_v1.py
+++ algo_v1.py
@@ -3,14 +3,31 @@ import pandas as pd
 df = pd.read_csv (r'sample_data4.csv',sep=";")
 print (df)
 
-#df = df.sort_values(['Gewicht'],
-#              ascending = [True])
+# df = df.sort_values(['Einlieferung'],
+#               ascending = [True])
 
-#df = df.sort_values(['Einlieferung', 'Gewicht'],
-#              ascending = [True, True])
+# df = df.sort_values(['Einlieferung', 'Gewicht'],
+#               ascending = [True, True])
 
 
-df.sort_values(by=['Einlieferung','Gewicht'], inplace=True,
+#split eillieferung to differrent df
+dfeil = df[df['Status'] == 1]
+#remove rows with status 1 from df
+df = df[df['Status'] != 1]
+#sort dfeil by Einlieferung
+dfeil = dfeil.sort_values(['Einlieferung'])
+print("Eillieferung:")
+print (dfeil)
+
+#sort df by Einlieferung and Gewicht
+df = df.sort_values(by=['Einlieferung', 'Gewicht'], kind='stable', 
                ascending = [True, True])
-
+print("Normal:")
 print(df)
+
+#define merge list
+frames = [dfeil, df]
+
+#merge dfs sorting x = dfeil, y = df
+dfmerge = pd.concat(frames, keys=["x","y"])
+print(dfmerge)

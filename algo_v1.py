821228fd998c58bed3e68b8e04ac9d2daf715342
lvlconcepts <leo.niss@student.fh-kiel.de>
2022-04-28 10:56:59 +0200

    guiv2

--- algo_v1.py
+++ algo_v1.py
@@ -28,3 +28,29 @@ print(dfmerge)
 
 #export dfmerge to xlsx
 #dfmerge.to_excel('output.xlsx', sheet_name='Sheet1')
+from tkinter import *
+root = Tk()
+root.title("Pakete")
+root.geometry("500x500")
+listbox_frame=Frame(root,borderwidth=5, relief=SUNKEN)
+listbox_frame.pack(pady=120)
+ 
+listbox=Listbox(listbox_frame, height=120)
+listbox.pack()
+
+listbox.insert(END,"Pakete")
+ 
+for item in [dfmerge["ID"]]:
+    listbox.insert(END, item)
+ 
+def list_item_selected():
+    selection=listbox.curselection()
+    if selection:
+        print(listbox.get(selection[0]))
+ 
+list_box_button = Button(listbox_frame,text='Paket auswählen', command=list_item_selected)
+list_box_button.pack()
+
+root.mainloop()
+
+dfmerge.loc[(dfmerge['Month'] == list_item_selected), ['Amount']].sum()

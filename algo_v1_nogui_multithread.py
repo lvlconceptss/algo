5487e66109d6a80093f7c50392ef77d042e998fb
lvlconcepts <leo.niss@student.fh-kiel.de>
2022-06-14 09:25:27 +0200

    multithread test

---
+++ algo_v1_nogui_multithread.py
@@ -0,0 +1,56 @@
+
+import modin.pandas as pd
+
+def init():
+    pd.DataFrame(columns=["Einlieferung","Gewicht","Status","ID"]).to_csv('resttag.csv', sep=";", index=False)
+
+#only use if repository is not initialized
+#init()
+
+try:
+    #import df from csv
+    dfnorm = pd.read_csv (r'sample_data4_large.csv',sep=";")
+    #import rest of df from csv
+    dfrest = pd.read_csv (r'resttag.csv',sep=";")
+except :
+    print("Es wurde eine falsche csv-Datei eingegeben, kontaktieren Sie Ihren Administrator!")
+
+def bearbeitung(dfold):
+    dfold.to_excel('Paketliste.xlsx', sheet_name='Pakete')
+    print("Pakete die heute für den Versand vorgesehen sind:  ", dfold.shape[0])
+    i = int(input("Bis zu welchem Paket wurde bearbeitet? "))
+    print(dfold)
+    dfold.drop(dfold.index[0:i], axis=0, inplace=True)
+    print(dfold)
+    dfold.to_csv('resttag.csv', sep=";", index=False)
+
+try:
+    ##Beginn Algorithmus
+    #split eillieferung to differrent df
+    dfeil = dfnorm[dfnorm['Status'] == 1]
+    #remove rows with status 1 from df
+    dfnorm = dfnorm[dfnorm['Status'] != 1]
+
+    #sort dfeil by Gewicht 
+    dfeil = dfeil.sort_values(['Gewicht'],ascending = [True])
+    print("Eil: ")
+    print (dfeil)
+
+    #sort dfnormal by Gewicht
+    dfnorm = dfnorm.sort_values(by=['Gewicht'], ascending = [True])
+    print("Normal:")
+    print(dfnorm)
+
+    #define merge list
+    frames = [dfrest, dfeil, dfnorm]
+
+    #merge dfs sorting x = dfeil, y = df
+    dfmerge = pd.concat(frames, keys=["Rest","Eil","Normal"])
+    print(dfmerge)
+    ##Ende Algorithmus
+
+    #Start der Bearbeitung durch Mitarbeiter
+    bearbeitung(dfmerge)
+
+except:
+    print("Es ist ein Fehler beim Sortieren aufgetreten, kontaktieren Sie Ihren Administrator!")
